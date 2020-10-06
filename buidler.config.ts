1d4979d659611e0d360a122e5afaed6c50402654
Jitendra Chittoda <jcchittoda@gmail.com>
2020-10-06 14:38:37 +0530

    Fixing build

--- buidler.config.ts
+++ buidler.config.ts
@@ -11,7 +11,7 @@ usePlugin("buidler-typechain");
 // defaultNetwork, networks, solc, and paths.
 // Go to https://buidler.dev/config/ to learn more
 export default {
-  defaultNetwork: "localhost",
+  defaultNetwork: "buidlerevm",
   networks: {
     buidlerevm: {
       allowUnlimitedContractSize: true,
@@ -39,108 +39,87 @@ export default {
         //   0x68dfc526037e9030c8f813d014919cc89e7d4d74
         //   0x26c43a1d431a4e5ee86cd55ed7ef9edf3641e901
         {
-          privateKey:
-            "0xc5e8f61d1ab959b397eecc0a37a6517b8e67a0e7cf1f4bce5591f3ed80199122",
+          privateKey: "0xc5e8f61d1ab959b397eecc0a37a6517b8e67a0e7cf1f4bce5591f3ed80199122",
           balance: "100000000000000000000000000000000",
         },
         {
-          privateKey:
-            "0xd49743deccbccc5dc7baa8e69e5be03298da8688a15dd202e20f15d5e0e9a9fb",
+          privateKey: "0xd49743deccbccc5dc7baa8e69e5be03298da8688a15dd202e20f15d5e0e9a9fb",
           balance: "100000000000000000000000000000000",
         },
         {
-          privateKey:
-            "0x23c601ae397441f3ef6f1075dcb0031ff17fb079837beadaf3c84d96c6f3e569",
+          privateKey: "0x23c601ae397441f3ef6f1075dcb0031ff17fb079837beadaf3c84d96c6f3e569",
           balance: "100000000000000000000000000000000",
         },
         {
-          privateKey:
-            "0xee9d129c1997549ee09c0757af5939b2483d80ad649a0eda68e8b0357ad11131",
+          privateKey: "0xee9d129c1997549ee09c0757af5939b2483d80ad649a0eda68e8b0357ad11131",
           balance: "100000000000000000000000000000000",
         },
         {
-          privateKey:
-            "0x87630b2d1de0fbd5044eb6891b3d9d98c34c8d310c852f98550ba774480e47cc",
+          privateKey: "0x87630b2d1de0fbd5044eb6891b3d9d98c34c8d310c852f98550ba774480e47cc",
           balance: "100000000000000000000000000000000",
         },
         {
-          privateKey:
-            "0x275cc4a2bfd4f612625204a20a2280ab53a6da2d14860c47a9f5affe58ad86d4",
+          privateKey: "0x275cc4a2bfd4f612625204a20a2280ab53a6da2d14860c47a9f5affe58ad86d4",
           balance: "100000000000000000000000000000000",
         },
         {
-          privateKey:
-            "0x7f307c41137d1ed409f0a7b028f6c7596f12734b1d289b58099b99d60a96efff",
+          privateKey: "0x7f307c41137d1ed409f0a7b028f6c7596f12734b1d289b58099b99d60a96efff",
           balance: "100000000000000000000000000000000",
         },
         {
-          privateKey:
-            "0x2a8aede924268f84156a00761de73998dac7bf703408754b776ff3f873bcec60",
+          privateKey: "0x2a8aede924268f84156a00761de73998dac7bf703408754b776ff3f873bcec60",
           balance: "100000000000000000000000000000000",
         },
         {
-          privateKey:
-            "0x8b24fd94f1ce869d81a34b95351e7f97b2cd88a891d5c00abc33d0ec9501902e",
+          privateKey: "0x8b24fd94f1ce869d81a34b95351e7f97b2cd88a891d5c00abc33d0ec9501902e",
           balance: "100000000000000000000000000000000",
         },
         {
-          privateKey:
-            "0x28d1bfbbafe9d1d4f5a11c3c16ab6bf9084de48d99fbac4058bdfa3c80b29085",
+          privateKey: "0x28d1bfbbafe9d1d4f5a11c3c16ab6bf9084de48d99fbac4058bdfa3c80b29085",
           balance: "100000000000000000000000000000000",
         },
         {
-          privateKey:
-            "0x28d1bfbbafe9d1d4f5a11c3c16ab6bf9084de48d99fbac4058bdfa3c80b29086",
+          privateKey: "0x28d1bfbbafe9d1d4f5a11c3c16ab6bf9084de48d99fbac4058bdfa3c80b29086",
           balance: "100000000000000000000000000000000",
         },
         {
-          privateKey:
-            "0x28d1bfbbafe9d1d4f5a11c3c16ab6bf9084de48d99fbac4058bdfa3c80b29087",
+          privateKey: "0x28d1bfbbafe9d1d4f5a11c3c16ab6bf9084de48d99fbac4058bdfa3c80b29087",
           balance: "100000000000000000000000000000000",
         },
         {
-          privateKey:
-            "0x28d1bfbbafe9d1d4f5a11c3c16ab6bf9084de48d99fbac4058bdfa3c80b29088",
+          privateKey: "0x28d1bfbbafe9d1d4f5a11c3c16ab6bf9084de48d99fbac4058bdfa3c80b29088",
           balance: "100000000000000000000000000000000",
         },
         {
-          privateKey:
-            "0x28d1bfbbafe9d1d4f5a11c3c16ab6bf9084de48d99fbac4058bdfa3c80b29089",
+          privateKey: "0x28d1bfbbafe9d1d4f5a11c3c16ab6bf9084de48d99fbac4058bdfa3c80b29089",
           balance: "100000000000000000000000000000000",
         },
         {
-          privateKey:
-            "0x28d1bfbbafe9d1d4f5a11c3c16ab6bf9084de48d99fbac4058bdfa3c80b2908a",
+          privateKey: "0x28d1bfbbafe9d1d4f5a11c3c16ab6bf9084de48d99fbac4058bdfa3c80b2908a",
           balance: "100000000000000000000000000000000",
         },
         {
-          privateKey:
-            "0x28d1bfbbafe9d1d4f5a11c3c16ab6bf9084de48d99fbac4058bdfa3c80b2908b",
+          privateKey: "0x28d1bfbbafe9d1d4f5a11c3c16ab6bf9084de48d99fbac4058bdfa3c80b2908b",
           balance: "100000000000000000000000000000000",
         },
         {
-          privateKey:
-            "0x28d1bfbbafe9d1d4f5a11c3c16ab6bf9084de48d99fbac4058bdfa3c80b2908c",
+          privateKey: "0x28d1bfbbafe9d1d4f5a11c3c16ab6bf9084de48d99fbac4058bdfa3c80b2908c",
           balance: "100000000000000000000000000000000",
         },
         {
-          privateKey:
-            "0x28d1bfbbafe9d1d4f5a11c3c16ab6bf9084de48d99fbac4058bdfa3c80b2908d",
+          privateKey: "0x28d1bfbbafe9d1d4f5a11c3c16ab6bf9084de48d99fbac4058bdfa3c80b2908d",
           balance: "100000000000000000000000000000000",
         },
         {
-          privateKey:
-            "0x28d1bfbbafe9d1d4f5a11c3c16ab6bf9084de48d99fbac4058bdfa3c80b2908e",
+          privateKey: "0x28d1bfbbafe9d1d4f5a11c3c16ab6bf9084de48d99fbac4058bdfa3c80b2908e",
           balance: "100000000000000000000000000000000",
         },
         {
-          privateKey:
-            "0x28d1bfbbafe9d1d4f5a11c3c16ab6bf9084de48d99fbac4058bdfa3c80b2908f",
+          privateKey: "0x28d1bfbbafe9d1d4f5a11c3c16ab6bf9084de48d99fbac4058bdfa3c80b2908f",
           balance: "100000000000000000000000000000000",
         },
       ],
     },
-    localhost: { url: "http://localhost:8545" },
   },
   // This is a sample solc configuration that specifies which version of solc to use
   solc: {
