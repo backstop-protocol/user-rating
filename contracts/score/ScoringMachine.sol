66331486857948d3a89ef695df5e242043120d6c
yaron velner <yaron172@yahoo.com>
2021-03-19 01:39:04 +0200

    contracts/score/ScoringMachine.sol

--- contracts/score/ScoringMachine.sol
+++ contracts/score/ScoringMachine.sol
@@ -5,79 +5,101 @@ import { Ownable } from "../../openzeppelin-contracts/contracts/ownership/Ownabl
 contract ScoringMachine is Ownable {
     struct AssetScore {
         // total score so far
-        uint score;
+        uint112 score;
 
         // current balance
-        uint balance;
+        uint112 balance;
 
         // time when last update was
-        uint last;
+        uint32 last;
+    }
+
+    struct User {
+        // Three epochs. One is final. One ended, but could be slashed. And one is current.
+        AssetScore[3] scores;
     }
 
     // user is bytes32 (will be the sha3 of address or cdp number)
-    mapping(bytes32 => mapping(bytes32 => AssetScore[])) public checkpoints;
-
-    mapping(bytes32 => mapping(bytes32 => AssetScore)) public userScore;
+    mapping(bytes32 => mapping(bytes32 => User)) public userScore;
 
     bytes32 constant public GLOBAL_USER = bytes32(0x0);
 
     uint public start; // start time of the campaign;
+    uint constant public EPOCH = 1 days; // TODO - decide
 
     function spin() external onlyOwner { // start a new round
         start = now;
     }
 
-    function assetScore(AssetScore storage score, uint time, uint spinStart) internal view returns(uint) {
-        uint last = score.last;
-        uint currentScore = score.score;
-        if(last < spinStart) {
-            last = spinStart;
-            currentScore = 0;
+    function getLatestScoreIndex(bytes32 user, bytes32 asset) internal view returns(uint latest, uint[3] last) {
+        uint last0 = userScore[user][asset][0].last;
+        uint last1 = userScore[user][asset][1].last;
+        uint last2 = userScore[user][asset][2].last;
+
+        last[0] = last0; last[1] = last1; last[2] = last2;
+
+        if((last0 > last1) && (last0 > last2)) latest = 0;
+        else if(last1 > last2) latest = 1;
+        else latest = 2;
+    }
+
+    function updateAssetScore(bytes32 user, bytes32 asset, int dbalance) internal {
+        (uint latest, uint[3] last) = getLatestScoreIndex(user, asset);
+        uint time = now;
+
+        uint dtime = sub(time, last[latest] == 0 ? start : last[latest]);
+        if(dtime >= EPOCH) {
+            latest = (latest + 1) % 3;
         }
+        
+        AssetScore storage score = userScore[user][asset][latest];
 
-        return add(currentScore, mul(score.balance, sub(time, last)));
-    }
-
-    function addCheckpoint(bytes32 user, bytes32 asset) internal {
-        checkpoints[user][asset].push(userScore[user][asset]);
-    }
-
-    function updateAssetScore(bytes32 user, bytes32 asset, int dbalance, uint time) internal {
-        AssetScore storage score = userScore[user][asset];
-
-        if(score.last < start) addCheckpoint(user, asset);
-
-        score.score = assetScore(score, time, start);
+        score.score = add(score.score, mul(score.balance, dtime));
         score.balance = add(score.balance, dbalance);
         
         score.last = time;
     }
 
-    function updateScore(bytes32 user, bytes32 asset, int dbalance, uint time) internal {
-        updateAssetScore(user, asset, dbalance, time);
-        updateAssetScore(GLOBAL_USER, asset, dbalance, time);
+    function slashAssetScore(byte32 user, bytes32 asset, int dbalance) internal {
+        (uint latest, uint[3] last) = getLatestScoreIndex(user, asset);
+        uint time = now;
+        
+        for(uint i = 0 ; i < 2 ; i++) {
+            uint index = (latest + 2 + i) % 3;
+            AssetScore storage score = userScore[user][asset][index];
+            score.balance = add(score.balance, dbalance);
+            
+            if(add(last[index], 2 * EPOCH) >= time) {
+                score.score = add(score.score, -mul(uint(-1 * dbalance), EPOCH));
+            }
+        }
     }
 
-    function getScore(bytes32 user, bytes32 asset, uint time, uint spinStart, uint checkPointHint) public view returns(uint score) {
-        if(time >= userScore[user][asset].last) return assetScore(userScore[user][asset], time, spinStart);
+    function updateScore(bytes32 user, bytes32 asset, int dbalance) internal {
+        updateAssetScore(user, asset, dbalance);
+        updateAssetScore(GLOBAL_USER, asset, dbalance);
+    }
 
-        // else - check the checkpoints
-        uint checkpointsLen = checkpoints[user][asset].length;
-        if(checkpointsLen == 0) return 0;
+    function slashScore(bytes32 user, bytes32 asset, int dbalance) internal {
+        slashAssetScore(user, asset, dbalance);
+        slashAssetScore(GLOBAL_USER, asset, dbalance);
+    }    
 
-        // hint is invalid
-        if(checkpoints[user][asset][checkPointHint].last < time) checkPointHint = checkpointsLen - 1;
-
-        for(uint i = checkPointHint ; ; i--){
-            if(checkpoints[user][asset][i].last <= time) return assetScore(checkpoints[user][asset][i], time, spinStart);
+    function getScore(bytes32 user, bytes32 asset, uint time) public view returns(uint score) {
+        (uint latest, uint[3] last) = getLatestScoreIndex(user, asset);
+        for(uint i = 0 ; i < 3 ; i++) {
+            uint index = (latest + i) % 3;
+            if(add(last[index], EPOCH) >= time) break;
         }
 
-        // this supposed to be unreachable
-        return 0;
+        AssetScore storage score = userScore[user][asset][index];
+        uint dtime = sub(time, latest[index]);
+        return add(score.score, mul(score.balance, dtime));
     }
 
     function getCurrentBalance(bytes32 user, bytes32 asset) public view returns(uint balance) {
-        balance = userScore[user][asset].balance;
+        (uint latest,) = getLatestScoreIndex(user, asset);
+        balance = userScore[user][asset][latest].balance;
     }
 
     // Math functions without errors
@@ -111,5 +133,36 @@ contract ScoringMachine is Ownable {
         if(!(z / x == y)) return 0;
 
         return z;
+    }
+}
+
+contract ClaimMachine is ScoringMachine {
+    bytes32 constant public INDEX_USER = bytes32(0x1);
+
+    struct UserIndex {
+        uint112 lastIndex;
+        uint32  lastClaimTime;
+    }
+
+    // user => asset > UserIndex
+    mapping(bytes32 => mapping(bytes32 => UserIndex)) userIndex;
+
+    function adjustSpeed(bytes32 asset, int dspeed) internal {
+        updateScore(INDEX_USER, asset, dspeed);
+    }
+
+    function claim(bytes32 user, bytes32 asset) internal returns(uint claimAmount) {
+        uint time = now % EPOCH - EPOCH;
+
+        uint totalScore = getScore(GLOBAL_USER, asset, time);
+        uint userScore = getScore(user, asset, time);
+
+        uint globalIndex = getScore(INDEX_USER, asset, time);
+
+        UserIndex storage index = userIndex[user][asset];
+        uint claimAmount = mul(userScore, mul(sub(globalIndex, index.lastIndex), sub(time, index.lastClaimTime))) / totalScore;
+
+        index.lastIndex = globalIndex;
+        lastClaimTime = time;
     }
 }
