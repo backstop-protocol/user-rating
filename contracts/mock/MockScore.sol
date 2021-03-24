c87744c06c76ef31c0a464020531fb062cf212e7
yaron velner <yaron172@yahoo.com>
2021-03-24 17:26:23 +0200

    gov index

--- contracts/mock/MockScore.sol
+++ contracts/mock/MockScore.sol
@@ -3,19 +3,19 @@ pragma solidity 0.5.16;
 import { ClaimMachine } from "../score/ScoringMachine.sol";
 
 contract MockScoreMachine is ClaimMachine {
-    function adjustSpeedMock(bytes32 asset, int112 dspeed, uint32 time) public {
-        super.adjustSpeed(asset, dspeed, time);
+    function adjustSpeedMock(bytes32 asset, int96 dspeed, uint32 time, uint96 slashSpeed) public {
+        super.adjustSpeed(asset, dspeed, time, slashSpeed);
     }
 
-    function claimMock(bytes32 user, bytes32 asset) public returns(uint112 claimAmount) {
+    function claimMock(bytes32 user, bytes32 asset) public returns(uint96 claimAmount) {
         claimAmount = super.claim(user,asset);
     }
 
-    function updateScoreMock(bytes32 user, bytes32 asset, int112 dbalance, uint32 time) public {
+    function updateScoreMock(bytes32 user, bytes32 asset, int96 dbalance, uint32 time) public {
         super.updateScore(user, asset, dbalance, time);
     }
 
-    function slashScoreMock(bytes32 user, bytes32 asset, int112 dbalance, uint32 time) public {
+    function slashScoreMock(bytes32 user, bytes32 asset, uint96 dbalance, uint32 time) public {
         super.slashScore(user, asset, dbalance, time);
     }
 }
--- contracts/score/ScoringMachine.sol
+++ contracts/score/ScoringMachine.sol
@@ -5,13 +5,16 @@ import { Ownable } from "../../openzeppelin-contracts/contracts/ownership/Ownabl
 contract ScoringMachine is Ownable {
     struct AssetScore {
         // total score so far
-        uint112 score;
+        uint96 score;
 
         // current balance
-        uint112 balance;
+        uint96 balance;
 
         // time when last update was
-        uint32 last;
+        uint32 lastTime;
+
+        // index value of gov
+        uint32 lastGovIndex;        
     }
 
     struct User {
@@ -36,9 +39,9 @@ contract ScoringMachine is Ownable {
         bytes32 asset
     ) internal view returns(uint latest, uint32[3] memory last) {
         User memory userData = userScore[user][asset];
-        uint32 last0 = userData.scores[0].last;
-        uint32 last1 = userData.scores[1].last;
-        uint32 last2 = userData.scores[2].last;
+        uint32 last0 = userData.scores[0].lastTime;
+        uint32 last1 = userData.scores[1].lastTime;
+        uint32 last2 = userData.scores[2].lastTime;
 
         last[0] = last0; last[1] = last1; last[2] = last2;
 
@@ -56,30 +59,33 @@ contract ScoringMachine is Ownable {
         return inEpoch(add32(t0, EPOCH), t1);
     }
 
-    function updateAssetScore(bytes32 user, bytes32 asset, int112 dbalance, uint32 time) internal {
+    function updateAssetScore(bytes32 user, bytes32 asset, int96 dbalance, uint32 time, uint32 govIndex) internal {
         (uint latest, uint32[3] memory last) = getLatestScoreIndex(user, asset);
-        uint32 t0 = last[latest] == 0 ? start : last[latest];
+        uint32 t0 = last[latest] == 0 ? time : last[latest];
 
         AssetScore storage score = userScore[user][asset].scores[latest];
-        uint112 scoreBefore = score.score;
-        uint112 balanceBefore = score.balance;
+        uint96 scoreBefore = score.score;
+        uint96 balanceBefore = score.balance;
+        uint32  govBefore = score.lastGovIndex;
 
-        uint32 dtime = sub32(time, t0);
+        uint32 dgov = sub32(govIndex, govBefore);
+
         if(! inEpoch(t0, time)) {
             latest = (latest + 1) % 3;
         }
         
         score = userScore[user][asset].scores[latest];
 
-        score.score = uint112(add(scoreBefore, mul(balanceBefore, dtime)));
-        score.balance = uint112(add(balanceBefore, dbalance));
+        score.score = uint96(add(scoreBefore, mul(balanceBefore, dgov)));
+        score.balance = uint96(add(balanceBefore, dbalance));
         
-        score.last = uint32(time);
+        score.lastTime = uint32(time);
+        score.lastGovIndex = govIndex;
     }
 
-    function slashAssetScore(bytes32 user, bytes32 asset, int112 dbalance, uint32 time) internal {
+    function slashAssetScore(bytes32 user, bytes32 asset, uint96 dbalance, uint32 time, uint32 govIndex, uint32 dgov) internal {
         // make sure there is a record current time, that is slashable
-        updateAssetScore(user, asset, 0, time);
+        updateAssetScore(user, asset, 0, time, govIndex);
 
         (uint latest, uint32[3] memory last) = getLatestScoreIndex(user, asset);
 
@@ -89,22 +95,22 @@ contract ScoringMachine is Ownable {
             if(! (inEpoch(last[index], time) || inPrevEpoch(last[index], time))) continue;
 
             AssetScore storage score = userScore[user][asset].scores[index];
-            score.balance = add(score.balance, dbalance);
-            score.score = add(score.score, -mul(uint112(-dbalance), EPOCH));
+            score.balance = add(score.balance, -int96(dbalance));
+            score.score = add(score.score, -int96(mul(dgov, dbalance)));
         }
     }
 
-    function updateScore(bytes32 user, bytes32 asset, int112 dbalance, uint32 time) internal {
-        updateAssetScore(user, asset, dbalance, time);
-        updateAssetScore(GLOBAL_USER, asset, dbalance, time);
+    function updateScore(bytes32 user, bytes32 asset, int96 dbalance, uint32 time, uint32 govIndex) internal {
+        updateAssetScore(user, asset, dbalance, time, govIndex);
+        updateAssetScore(GLOBAL_USER, asset, dbalance, time, govIndex);
     }
 
-    function slashScore(bytes32 user, bytes32 asset, int112 dbalance, uint32 time) internal {
-        slashAssetScore(user, asset, dbalance, time);
-        slashAssetScore(GLOBAL_USER, asset, dbalance, time);
+    function slashScore(bytes32 user, bytes32 asset, uint96 dbalance, uint32 time, uint32 govIndex, uint32 dgov) internal {
+        slashAssetScore(user, asset, dbalance, time, govIndex, dgov);
+        slashAssetScore(GLOBAL_USER, asset, dbalance, time, govIndex, dgov);
     }    
 
-    function getScore(bytes32 user, bytes32 asset, uint32 time) public view returns(uint112 score) {
+    function getScore(bytes32 user, bytes32 asset, uint32 time) public view returns(uint96 score) {
         (uint latest, uint32[3] memory last) = getLatestScoreIndex(user, asset);
         uint index = 0;
         for(uint i = 0 ; i < 3 ; i++) {
@@ -117,26 +123,26 @@ contract ScoringMachine is Ownable {
         return add(currScore.score, mul(currScore.balance, dtime));
     }
 
-    function getGlobalScore(bytes32 asset, uint32 time) public view returns(uint112 score) {
+    function getGlobalScore(bytes32 asset, uint32 time) public view returns(uint96 score) {
         return getScore(GLOBAL_USER, asset, time);
     }
 
-    function getCurrentBalance(bytes32 user, bytes32 asset) public view returns(uint112 balance) {
+    function getCurrentBalance(bytes32 user, bytes32 asset) public view returns(uint96 balance) {
         (uint latest,) = getLatestScoreIndex(user, asset);
         balance = userScore[user][asset].scores[latest].balance;
     }
 
     // Math functions without errors
     // ==============================
-    function add(uint112 x, uint112 y) internal pure returns (uint112 z) {
+    function add(uint96 x, uint96 y) internal pure returns (uint96 z) {
         z = x + y;
         if(!(z >= x)) return 0;
 
         return z;
     }
 
-    function add(uint112 x, int112 y) internal pure returns (uint112 z) {
-        z = x + uint112(y);
+    function add(uint96 x, int96 y) internal pure returns (uint96 z) {
+        z = x + uint96(y);
         if(!(y >= 0 || z <= x)) return 0;
         if(!(y <= 0 || z >= x)) return 0;
 
@@ -150,7 +156,7 @@ contract ScoringMachine is Ownable {
         return z;
     }
 
-    function sub(uint112 x, uint112 y) internal pure returns (uint112 z) {
+    function sub(uint96 x, uint96 y) internal pure returns (uint96 z) {
         if(!(y <= x)) return 0;
         z = x - y;
 
@@ -164,7 +170,7 @@ contract ScoringMachine is Ownable {
         return z;
     }    
 
-    function mul(uint112 x, uint112 y) internal pure returns (uint112 z) {
+    function mul(uint96 x, uint96 y) internal pure returns (uint96 z) {
         if (x == 0) return 0;
 
         z = x * y;
@@ -178,12 +184,12 @@ contract ClaimMachine is ScoringMachine {
     bytes32 constant public GOV_USER = bytes32(uint(0x1));
 
     struct UserClaimInfo {
-        uint112 scoreClaimed;
+        uint96 scoreClaimed;
     }
 
     struct ClaimInfo {
-        uint112 globalScoreClaimed;
-        uint112 govClaimed;
+        uint96 globalScoreClaimed;
+        uint96 govClaimed;
 
         // user => user claim info
         mapping(bytes32 => UserClaimInfo) userInfo;
@@ -192,26 +198,41 @@ contract ClaimMachine is ScoringMachine {
     // asset => claimInfo
     mapping(bytes32 => ClaimInfo) claimInfo;
 
-    function adjustSpeed(bytes32 asset, int112 dspeed, uint32 time) internal {
-        updateScore(GOV_USER, asset, dspeed, time);
+    // asset => slash speed
+    mapping(bytes32 => uint96) slashSpeed;
+
+    function adjustSpeed(bytes32 asset, int96 dspeed, uint32 time, uint96 newSlashSpeed) internal {
+        super.updateScore(GOV_USER, asset, dspeed, time, time);
+        slashSpeed[asset] = newSlashSpeed;
     }
 
-    function getGovScore(bytes32 asset, uint32 time) public view returns(uint112 score) {
+    function getGovScore(bytes32 asset, uint32 time) public view returns(uint96 score) {
         return getScore(GOV_USER, asset, time);
     }
 
-    function claim(bytes32 user, bytes32 asset) internal returns(uint112 claimAmount) {
+    function updateScore(bytes32 user, bytes32 asset, int96 dbalance, uint32 time) internal {
+        // if there is an overflow, then sfyl
+        uint32 govIndex = uint32(getGovScore(asset, time));
+        super.updateScore(user, asset, dbalance, time, govIndex);
+    }
+
+    function slashScore(bytes32 user, bytes32 asset, uint96 dbalance, uint32 time) internal {
+        // if there is an overflow, then sfyl
+        uint32 govIndex = uint32(getGovScore(asset, time));
+        uint32 dgov = uint32(mul(slashSpeed[asset], 2 * EPOCH));
+        super.slashScore(user, asset, dbalance, time, govIndex, dgov);
+    }
+
+    function claim(bytes32 user, bytes32 asset) internal returns(uint96 claimAmount) {
         uint32 time = uint32(now % EPOCH - EPOCH);
 
-        uint112 totalScore = getGlobalScore(asset, time);
-        uint112 userScore = getScore(user, asset, time);
-        uint112 totalGov = getGovScore(asset, time);
+        uint96 totalScore = getGlobalScore(asset, time);
+        uint96 userScore = getScore(user, asset, time);
 
-        uint112 globalScoreClaimed = claimInfo[asset].globalScoreClaimed;
-        uint112 totalGovClaimed = claimInfo[asset].govClaimed;
-        uint112 userScoreClaimed = claimInfo[asset].userInfo[user].scoreClaimed;
+        uint96 globalScoreClaimed = claimInfo[asset].globalScoreClaimed;
+        uint96 userScoreClaimed = claimInfo[asset].userInfo[user].scoreClaimed;
 
-        claimAmount = mul(sub(userScore, userScoreClaimed), sub(totalGov, totalGovClaimed)) / sub(totalScore, globalScoreClaimed);
+        claimAmount = sub(userScore, userScoreClaimed) / sub(totalScore, globalScoreClaimed);
 
         claimInfo[asset].globalScoreClaimed = add(claimInfo[asset].globalScoreClaimed , userScore);
         claimInfo[asset].userInfo[user].scoreClaimed = add(claimInfo[asset].userInfo[user].scoreClaimed, userScore);        
