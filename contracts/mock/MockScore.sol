08c800a08bfe732e75d14e7bf3a69b4c2764948a
yaron velner <yaron172@yahoo.com>
2021-03-29 18:29:52 +0300

    mock score

--- contracts/mock/MockScore.sol
+++ contracts/mock/MockScore.sol
@@ -1,21 +1,18 @@
 pragma solidity 0.5.16;
 
-import { ClaimMachine } from "../score/ScoringMachine.sol";
 
-contract MockScoreMachine is ClaimMachine {
-    function adjustSpeedMock(bytes32 asset, int96 dspeed, uint32 time, uint96 slashSpeed) public {
-        super.adjustSpeed(asset, dspeed, time, slashSpeed);
+import { ScoringMachine } from "../score/ScoringMachine.sol";
+
+contract MockScoreMachine is ScoringMachine {
+    function setSpeedMock(bytes32 asset, uint speed, uint scoreNormFactor, uint blockNumber) public {
+        super.setSpeed(asset, speed, scoreNormFactor, blockNumber);
     }
 
-    function claimMock(bytes32 user, bytes32 asset) public returns(uint96 claimAmount) {
-        claimAmount = super.claim(user,asset);
+    function claimScoreMock(bytes32 user, bytes32 asset, uint96 expectedBalance, uint32 blockNumber) public returns(uint96 score) {
+        score = super.claimScore(user,asset,expectedBalance,blockNumber);
     }
 
-    function updateScoreMock(bytes32 user, bytes32 asset, int96 dbalance, uint32 time) public {
-        super.updateScore(user, asset, dbalance, time);
-    }
-
-    function slashScoreMock(bytes32 user, bytes32 asset, uint96 dbalance, uint32 time) public {
-        super.slashScore(user, asset, dbalance, time);
+    function updateScoreMock(bytes32 user, bytes32 asset, int96 dbalance, uint96 expectedBalance, uint32 blockNumber) public {
+        super.updateAssetScore(user, asset, dbalance, expectedBalance, blockNumber);
     }
 }
