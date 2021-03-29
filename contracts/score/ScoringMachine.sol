b2b958e953b1f0e4ae908ea18d5df2721a3181c0
yaron velner <yaron172@yahoo.com>
2021-03-29 18:23:37 +0300

    balance based score

--- contracts/score/ScoringMachine.sol
+++ contracts/score/ScoringMachine.sol
@@ -3,239 +3,202 @@ pragma solidity ^0.5.12;
 import { Ownable } from "../../openzeppelin-contracts/contracts/ownership/Ownable.sol";
 
 contract ScoringMachine is Ownable {
-    struct AssetScore {
+    uint96 constant INDEX_FACTOR = uint96(1e10); // the basic unit will be 1e(-8) score
+
+    struct AssetUserData {
         // total score so far
-        uint96 score;
+        uint96 score; // up to 10Be18
 
         // current balance
+        uint96 balance; // up to 10Be18
+
+        // index of score distribution
+        uint64 lastScoreIndex; // up to 1e18   
+    }
+
+    struct AssetGlobalData {
         uint96 balance;
-
-        // time when last update was
-        uint32 lastTime;
-
-        // index value of gov
-        uint32 lastGovIndex;        
+        uint64 speed;
+        uint64 scoreIndex;
+        uint32 lastUpdateBlock;
     }
 
-    struct User {
-        // Three epochs. One is final. One ended, but could be slashed. And one is current.
-        AssetScore[3] scores;
+    struct AssetData {
+        AssetGlobalData globalData;
+        // mapping from user to data
+        mapping(bytes32 => AssetUserData) userData;
     }
 
-    // user is bytes32 (will be the sha3 of address or cdp number)
-    mapping(bytes32 => mapping(bytes32 => User)) internal userScore;
+    struct DistributionData {
+        uint speed;
+        uint scoreNormFactor;
+        uint lastSpeedUpdateBlock;
+        uint totalDistributed;
 
-    bytes32 constant public GLOBAL_USER = bytes32(0x0);
-
-    uint32 public start; // start time of the campaign;
-    uint32 constant public EPOCH = 1 days; // TODO - decide
-
-    function spin() external onlyOwner { // start a new round
-        start = uint32(now);
+        // how much each user already claimed
+        mapping(bytes32 => uint) claimed;
     }
 
-    function getLatestScoreIndex(
-        bytes32 user,
-        bytes32 asset
-    ) internal view returns(uint latest, uint32[3] memory last) {
-        User memory userData = userScore[user][asset];
-        uint32 last0 = userData.scores[0].lastTime;
-        uint32 last1 = userData.scores[1].lastTime;
-        uint32 last2 = userData.scores[2].lastTime;
+    // mapping from asset to data
+    mapping(bytes32 => AssetData) internal assetData;
 
-        last[0] = last0; last[1] = last1; last[2] = last2;
+    // mapping form asset to distribution data
+    mapping(bytes32 => DistributionData) internal assetDistributionData;
 
-        if((last0 > last1) && (last0 > last2)) latest = 0;
-        else if(last1 > last2) latest = 1;
-        else latest = 2;
+    function setSpeed(bytes32 asset, uint speed, uint scoreNormFactor, uint blockNumber) internal {
+        require(blockNumber <= uint32(-1), "setSpeed: blockNumber-overflow");
+        updateAssetScore(bytes32(0), asset, 0, 0, uint32(blockNumber));
+
+        require(speed <= uint64(-1), "setSpeed: speed-overflow");        
+        assetData[asset].globalData.speed = uint64(speed);
+
+        DistributionData storage data = assetDistributionData[asset];
+
+        require(blockNumber >= data.lastSpeedUpdateBlock, "setSpeed: blockNumber-overflow");
+        uint newlyDistributed = (blockNumber - data.lastSpeedUpdateBlock) * data.speed;
+        require(newlyDistributed <= uint128(-1),"setSpeed: newly-distributed-overflow");
+
+        require(data.totalDistributed <= uint128(-1), "setSpeed: totalDistributed overflow");
+        data.totalDistributed += newlyDistributed;
+        data.speed = speed;
+
+        require(scoreNormFactor <= uint96(-1), "setSpeed: scoreNormFactor-overflow");
+        data.scoreNormFactor = scoreNormFactor;
     }
 
-    function inEpoch(uint32 t0, uint32 t1) internal pure returns(bool) {
-        uint32 t0Epoch = (t0 / EPOCH) * EPOCH;
-        return add32(t0Epoch, EPOCH) > t1;
+    function slashAssetScore(AssetUserData storage userData, AssetGlobalData storage globalData, uint96 balanceDiff) internal {
+        uint64 scoreIndexDiff = sub64(globalData.scoreIndex, userData.lastScoreIndex);
+        uint96 slashedScore = mul96(uint96(scoreIndexDiff), balanceDiff);
+
+        uint96 slashedIndex = slashedScore / globalData.balance;
+        require(slashedIndex <= uint64(-1), "slashAssetScore: slashedInex-overflow");
+
+        globalData.scoreIndex = add64(globalData.scoreIndex, uint64(slashedIndex));
+
+        userData.balance = sub96(userData.balance, balanceDiff);
+        globalData.balance = sub96(globalData.balance, balanceDiff);
     }
 
-    function inPrevEpoch(uint32 t0, uint32 t1) internal pure returns(bool) {
-        return inEpoch(add32(t0, EPOCH), t1);
-    }
+    function updateAssetScore(bytes32 user, bytes32 asset, int96 dbalance, uint96 expectedBalance, uint32 blockNumber) internal {
+        AssetGlobalData storage globalData = assetData[asset].globalData;
+        AssetUserData storage userData = assetData[asset].userData[user];
 
-    function updateAssetScore(bytes32 user, bytes32 asset, int96 dbalance, uint32 time, uint32 govIndex) internal {
-        (uint latest, uint32[3] memory last) = getLatestScoreIndex(user, asset);
-        uint32 t0 = last[latest] == 0 ? time : last[latest];
-
-        AssetScore storage score = userScore[user][asset].scores[latest];
-        uint96 scoreBefore = score.score;
-        uint96 balanceBefore = score.balance;
-        uint32  govBefore = score.lastGovIndex;
-
-        uint32 dgov = sub32(govIndex, govBefore);
-
-        if(! inEpoch(t0, time)) {
-            latest = (latest + 1) % 3;
+        uint96 expectedBalanceBeforeUpdate = add96(expectedBalance, dbalance);
+        if(expectedBalanceBeforeUpdate < userData.balance) {
+            slashAssetScore(userData, globalData, sub96(userData.balance, expectedBalanceBeforeUpdate));
         }
+
+        // this supports user who entered before the upgrade
+        if(userData.balance == 0) dbalance = int96(expectedBalance);
+
+        uint64 scoreIndexDiff = mulmulmuldiv(globalData.speed, INDEX_FACTOR, sub32(blockNumber, globalData.lastUpdateBlock), globalData.balance);
+        globalData.scoreIndex = add64(globalData.scoreIndex, scoreIndexDiff);
+        globalData.balance = add96(globalData.balance, dbalance);
+        globalData.lastUpdateBlock = blockNumber;
+
+        // update user data
+        uint96 userScoreDiff = mul96(sub64(globalData.scoreIndex, userData.lastScoreIndex), userData.balance);
+        userData.score = add96(userData.score, userScoreDiff);
+        userData.lastScoreIndex = globalData.scoreIndex;
+        userData.balance = add96(userData.balance, dbalance);
+    }
+
+    function getScore(bytes32 user, bytes32 asset) public view returns(uint96 score) {
+        AssetGlobalData storage globalData = assetData[asset].globalData;
+        AssetUserData storage userData = assetData[asset].userData[user];
+
+        uint96 userScore = add96(userData.score, mul96(sub64(globalData.scoreIndex, userData.lastScoreIndex), userData.balance));
+        return mul96(userScore, uint96(assetDistributionData[asset].scoreNormFactor));
+    }
+
+    function getGlobalScore(bytes32 asset, uint32 blockNumber) public view returns(uint96 score) {
+        DistributionData storage data = assetDistributionData[asset];
+        require(data.totalDistributed <= uint96(-1), "getGlobalScore: totalDistributed-overflow");
+        require(blockNumber >= data.lastSpeedUpdateBlock, "getGlobalScore: blockNumber-overflow");
+        require(data.speed <= uint64(-1), "getGlobalScore: speed-overflow");
+
+        uint globalScore = data.totalDistributed + (blockNumber - data.lastSpeedUpdateBlock) * data.speed;
         
-        score = userScore[user][asset].scores[latest];
+        require(data.scoreNormFactor <= uint128(-1), "getGlobalScore: scoreNormFactor-overflow");
+        require(globalScore <= uint128(-1), "getGlobalScore: globalScore-overflow");
 
-        score.score = uint96(add(scoreBefore, mul(balanceBefore, dgov)));
-        score.balance = uint96(add(balanceBefore, dbalance));
-        
-        score.lastTime = uint32(time);
-        score.lastGovIndex = govIndex;
-    }
-
-    function slashAssetScore(bytes32 user, bytes32 asset, uint96 dbalance, uint32 time, uint32 govIndex, uint32 dgov) internal {
-        // make sure there is a record current time, that is slashable
-        updateAssetScore(user, asset, 0, time, govIndex);
-
-        (uint latest, uint32[3] memory last) = getLatestScoreIndex(user, asset);
-
-        for(uint i = 0 ; i < 2 ; i++) {
-            uint index = (latest + 2 + i) % 3;
-
-            if(! (inEpoch(last[index], time) || inPrevEpoch(last[index], time))) continue;
-
-            AssetScore storage score = userScore[user][asset].scores[index];
-            score.balance = add(score.balance, -int96(dbalance));
-            score.score = add(score.score, -int96(mul(dgov, dbalance)));
-        }
-    }
-
-    function updateScore(bytes32 user, bytes32 asset, int96 dbalance, uint32 time, uint32 govIndex) internal {
-        updateAssetScore(user, asset, dbalance, time, govIndex);
-        updateAssetScore(GLOBAL_USER, asset, dbalance, time, govIndex);
-    }
-
-    function slashScore(bytes32 user, bytes32 asset, uint96 dbalance, uint32 time, uint32 govIndex, uint32 dgov) internal {
-        slashAssetScore(user, asset, dbalance, time, govIndex, dgov);
-        slashAssetScore(GLOBAL_USER, asset, dbalance, time, govIndex, dgov);
-    }    
-
-    function getScore(bytes32 user, bytes32 asset, uint32 time) public view returns(uint96 score) {
-        (uint latest, uint32[3] memory last) = getLatestScoreIndex(user, asset);
-        uint index = 0;
-        for(uint i = 0 ; i < 3 ; i++) {
-            index = (latest + 1 + i) % 3;
-            if(inEpoch(last[index], time)) break;
-        }
-
-        AssetScore storage currScore = userScore[user][asset].scores[index];
-        uint32 dtime = sub32(time, last[index]);
-        return add(currScore.score, mul(currScore.balance, dtime));
-    }
-
-    function getGlobalScore(bytes32 asset, uint32 time) public view returns(uint96 score) {
-        return getScore(GLOBAL_USER, asset, time);
+        uint result = data.scoreNormFactor * globalScore;
+        require(result <= uint96(-1), "getGlobalScore: score-overflow");
+        score = uint96(result);
     }
 
     function getCurrentBalance(bytes32 user, bytes32 asset) public view returns(uint96 balance) {
-        (uint latest,) = getLatestScoreIndex(user, asset);
-        balance = userScore[user][asset].scores[latest].balance;
+        return assetData[asset].userData[user].balance;
     }
 
-    // Math functions without errors
+    function claimScore(bytes32 user, bytes32 asset, uint96 expectedBalance, uint32 blockNumber) internal returns(uint96 score) {
+        updateAssetScore(user, asset, 0, expectedBalance, blockNumber);
+        score = getScore(user, asset);
+
+        // cannot have overflow, as summing 96 bit integer
+        assetDistributionData[asset].claimed[user] += uint(score);
+    }
+
+    // Math functions
     // ==============================
-    function add(uint96 x, uint96 y) internal pure returns (uint96 z) {
-        z = x + y;
-        if(!(z >= x)) return 0;
-
-        return z;
+    function add96(uint96 x, uint96 y) internal pure returns (uint96 z) {
+        uint result = uint(x) + uint(y);
+        require(result <= uint96(-1), "add96: overflow");
+        z = uint96(result);
     }
 
-    function add(uint96 x, int96 y) internal pure returns (uint96 z) {
-        z = x + uint96(y);
-        if(!(y >= 0 || z <= x)) return 0;
-        if(!(y <= 0 || z >= x)) return 0;
+    function add96(uint96 x, int96 y) internal pure returns (uint96 z) {
+        int result = int(x) + int(y);
+        require(result >= 0, "add96: underflow");
+        require(result <= uint96(-1), "add96: overflow");
 
-        return z;
+        z = uint96(uint(result));
     }
 
-    function add32(uint32 x, uint32 y) internal pure returns (uint32 z) {
-        z = x + y;
-        if(!(z >= x)) return 0;
-
-        return z;
+    function add64(uint64 x, uint64 y) internal pure returns (uint64 z) {
+        uint result = uint(x) + uint(y);
+        require(result <= uint64(-1), "add96: overflow");
+        z = uint64(result);        
     }
 
-    function sub(uint96 x, uint96 y) internal pure returns (uint96 z) {
-        if(!(y <= x)) return 0;
+    function add64(uint64 x, int64 y) internal pure returns (uint64 z) {
+        int result = int(x) + int(y);
+        require(result >= 0, "add64: underflow");
+        require(result <= uint64(-1), "add96: overflow");
+
+        z = uint64(uint(result));
+    }
+
+    function sub96(uint96 x, uint96 y) internal pure returns (uint96 z) {
+        require(x >= y, "sub96: overflow");
         z = x - y;
 
         return z;
+    }
+
+    function sub64(uint64 x, uint64 y) internal pure returns (uint64 z) {
+        z = uint64(sub96(uint96(x), uint96(y)));
     }
 
     function sub32(uint32 x, uint32 y) internal pure returns (uint32 z) {
-        if(!(y <= x)) return 0;
-        z = x - y;
-
-        return z;
+        z = uint32(sub96(uint96(x), uint96(y)));
     }    
 
-    function mul(uint96 x, uint96 y) internal pure returns (uint96 z) {
+    function mul96(uint96 x, uint96 y) internal pure returns (uint96 z) {
         if (x == 0) return 0;
 
-        z = x * y;
-        if(!(z / x == y)) return 0;
+        uint result = uint(x) * uint(y);
+        require((result / uint(x)) == uint(y), "mul96: overlow");
+        require(result <= uint96(-1), "mul96: overflow");
 
-        return z;
-    }
-}
-
-contract ClaimMachine is ScoringMachine {
-    bytes32 constant public GOV_USER = bytes32(uint(0x1));
-
-    struct UserClaimInfo {
-        uint96 scoreClaimed;
+        z = uint96(result);
     }
 
-    struct ClaimInfo {
-        uint96 globalScoreClaimed;
-        uint96 govClaimed;
+    function mulmulmuldiv(uint64 x, uint96 y, uint32 z, uint96 w) internal pure returns(uint64 r) {
+        // x * y * z is atmost 192 bits, so can't have an overflow
+        uint result = uint(x) * uint(y) * uint(z) / uint(w);
 
-        // user => user claim info
-        mapping(bytes32 => UserClaimInfo) userInfo;
-    }
-
-    // asset => claimInfo
-    mapping(bytes32 => ClaimInfo) claimInfo;
-
-    // asset => slash speed
-    mapping(bytes32 => uint96) slashSpeed;
-
-    function adjustSpeed(bytes32 asset, int96 dspeed, uint32 time, uint96 newSlashSpeed) internal {
-        super.updateScore(GOV_USER, asset, dspeed, time, time);
-        slashSpeed[asset] = newSlashSpeed;
-    }
-
-    function getGovScore(bytes32 asset, uint32 time) public view returns(uint96 score) {
-        return getScore(GOV_USER, asset, time);
-    }
-
-    function updateScore(bytes32 user, bytes32 asset, int96 dbalance, uint32 time) internal {
-        // if there is an overflow, then sfyl
-        uint32 govIndex = uint32(getGovScore(asset, time));
-        super.updateScore(user, asset, dbalance, time, govIndex);
-    }
-
-    function slashScore(bytes32 user, bytes32 asset, uint96 dbalance, uint32 time) internal {
-        // if there is an overflow, then sfyl
-        uint32 govIndex = uint32(getGovScore(asset, time));
-        uint32 dgov = uint32(mul(slashSpeed[asset], 2 * EPOCH));
-        super.slashScore(user, asset, dbalance, time, govIndex, dgov);
-    }
-
-    function claim(bytes32 user, bytes32 asset) internal returns(uint96 claimAmount) {
-        uint32 time = uint32((now / EPOCH) * EPOCH - EPOCH);
-
-        uint96 totalScore = getGlobalScore(asset, time);
-        uint96 userScore = getScore(user, asset, time);
-
-        uint96 globalScoreClaimed = claimInfo[asset].globalScoreClaimed;
-        uint96 userScoreClaimed = claimInfo[asset].userInfo[user].scoreClaimed;
-
-        claimAmount = sub(userScore, userScoreClaimed) / sub(totalScore, globalScoreClaimed);
-
-        claimInfo[asset].globalScoreClaimed = add(claimInfo[asset].globalScoreClaimed , userScore);
-        claimInfo[asset].userInfo[user].scoreClaimed = add(claimInfo[asset].userInfo[user].scoreClaimed, userScore);        
-        claimInfo[asset].govClaimed = add(claimInfo[asset].govClaimed, claimAmount);
+        require(result <= uint64(-1), "mulmulmuldiv: overflow");
+        r = uint64(r);
     }
 }
